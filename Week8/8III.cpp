5a094565b0e7fc9a69b0bb44ca70ccd43db19f72
Kapil Chand <kapilchand750@gmail.com>
2026-04-05 21:16:14 +0530

    Added Week3 (Ques 3) Max MST using Prim's

--- Week8/8III.cpp
+++ Week8/8III.cpp
@@ -1,14 +1,14 @@
-// Week 8 (Ques 1) : Implementation of Prim's O((V+E)logV)
+// Week 8 (Ques 3) : Implementation of Prim's to find Max MST O((V+E)logV)
 
 #include<bits/stdc++.h>
 using namespace std;
 
-int prims(vector<vector<pair<int,int>>>& adj, int V, int src){
+int maxPrims(vector<vector<pair<int,int>>>& adj, int V, int src){
     
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    priority_queue<pair<int,int>> pq; // MAX HEAP
     
-    vector<int> key(V, INT_MAX);     // min edge weight
-    vector<int> parent(V, -1);       // MST structure
+    vector<int> key(V, INT_MIN);   // max edge weight
+    vector<int> parent(V, -1);     // MST structure
     vector<bool> visited(V, false);
     
     key[src] = 0;
@@ -32,7 +32,7 @@ int prims(vector<vector<pair<int,int>>>& adj, int V, int src){
             int adjNode = ngh.second;
             int adjWeight = ngh.first;
             
-            if(!visited[adjNode] && adjWeight < key[adjNode]){
+            if(!visited[adjNode] && adjWeight > key[adjNode]){
                 key[adjNode] = adjWeight;
                 parent[adjNode] = node;
                 pq.push({key[adjNode], adjNode});
@@ -41,7 +41,7 @@ int prims(vector<vector<pair<int,int>>>& adj, int V, int src){
     }
     
     // 🔹 Print MST edges
-    cout << "\nEdges in MST:\n";
+    cout << "\nEdges in MAX MST:\n";
     for(int i = 0; i < V; i++){
         if(parent[i] != -1){
             cout << parent[i] << " - " << i 
@@ -49,13 +49,13 @@ int prims(vector<vector<pair<int,int>>>& adj, int V, int src){
         }
     }
     
-    // 🔹 Print Parent Array
+    // 🔹 Parent Array
     cout << "\nParent Array:\n";
     for(int i = 0; i < V; i++){
         cout << "Node " << i << " -> Parent: " << parent[i] << "\n";
     }
     
-    // 🔹 Print Key Array
+    // 🔹 Key Array
     cout << "\nKey Array:\n";
     for(int i = 0; i < V; i++){
         cout << "Node " << i << " -> Key: " << key[i] << "\n";
@@ -75,12 +75,10 @@ int main(){
         cin >> u >> v >> w;
 
         adj[u].push_back({w, v});
-        adj[v].push_back({w, u}); // correct for undirected graph
+        adj[v].push_back({w, u});
     }
 
-    int cost = prims(adj, V, 0);
+    int cost = maxPrims(adj, V, 0);
     
-    cout << "\nTotal Minimum Cost (MST): " << cost << endl;
-    
-    return 0;
+    cout << "\nTotal Maximum Cost (MST): " << cost << endl;
 }
